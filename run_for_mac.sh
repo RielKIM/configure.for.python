627445f3d1dcb7d3afd69cbbc9657c9d7cf48234
YoungWook KIM <ukng1024@MacBook-Pro-2.local>
2019-03-05 11:34:31 +0900

    change script type.

--- run_for_mac.sh
+++ run_for_mac.sh
@@ -2,9 +2,9 @@
 
 current=$PWD
 
+brew install ctags-exuberant
 mkdir -p ~/.vim
 cp -R $current/syntax $current/ftplugin ~/.vim
-apt install exuberant-ctags
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp vimrc.tmp ~/.vimrc
--- run_for_ubuntu.sh
+++ run_for_ubuntu.sh
@@ -4,7 +4,12 @@ current=$PWD
 
 mkdir -p ~/.vim
 cp -R $current/syntax $current/ftplugin ~/.vim
-sudo apt install exuberant-ctags
+
+if [ "$(id -u)" != "0" ]; then
+	sudo apt install exuberant-ctags
+else
+	apt install exuberant-ctags
+fi
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp vimrc.tmp ~/.vimrc
